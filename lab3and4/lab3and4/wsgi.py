3e20906664d89ca095327d370b4e46ff6e0d52f1
Alexander Hans Collado Jr <lex.collado@gmail.com>
2018-04-30 14:02:36 +0800

    add lab 3 and 4

--- lab3and4/lab3and4/wsgi.py
+++ lab3and4/lab3and4/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for lab_3 project.
+WSGI config for lab3and4 project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lab3.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lab3and4.settings")
 
 application = get_wsgi_application()
